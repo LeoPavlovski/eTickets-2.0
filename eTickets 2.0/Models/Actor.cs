603d6131698e5cac5f8372096d80bd0ccc42645f
LeoPavlovski <lp29379@seeu.edu.mk>
2023-02-10 22:39:30 +0100

    first commit

---
+++ eTickets 2.0/Models/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets_2._0.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProfilePictureURL { get; set; }
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+
+        public List<Actor_Movie> Actors_Movies { get; set; }
+    
+    }
+
+}
